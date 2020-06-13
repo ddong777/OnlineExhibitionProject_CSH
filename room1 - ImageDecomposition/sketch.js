13b8d8e92292b1f0022cbb8da6bdf838f51d2a35
ddong777 <dj060352@naver.com>
2020-06-13 14:44:42 +0900

    add img uploading code in room1
    뭔가 세포들이 있는 판 위에 이미지를 올리고 그위에 자라나도록... 예제 https://p5js.org/examples/dom-drop.html 코드 이용

--- room1 - ImageDecomposition/sketch.js
+++ room1 - ImageDecomposition/sketch.js
@@ -6,10 +6,17 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(img.width, img.height);
+  const c = createCanvas(windowWidth, windowHeight);
   background(255);
   imageMode(CENTER);
   image(img, width / 2, height / 2);
+  c.drop(gotFile);
+
+  fill(0);
+  noStroke();
+  textSize(width/36);
+  textAlign(CENTER);
+  text('Drag an image file onto the canvas.', width / 2, height * 0.1);
 
   if ( width > height ) {
     for (let i = 0; i < width*5; i++) {
@@ -22,6 +29,22 @@ function setup() {
   }
 
   // print(img.width, img.height);
+}
+
+function gotFile(file) {
+  // If it's an image file
+  if (file.type === 'image') {
+    // Create an image DOM element but don't show it
+    const img = createImg(file.data).hide();
+    // Draw the image onto the canvas
+    if (width <  height/img.height*img.width){
+      image(img, width/2, height/2, width * 0.9, width/img.width*img.height * 0.9);
+      }
+    else{
+      image(img, width/2, height/2, height/img.height*img.width * 0.9, height * 0.9);}
+  } else {
+    console.log('Not an image file!');
+  }
 }
 
 function draw() {
